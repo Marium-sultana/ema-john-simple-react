2cc96bdd793656c91dfa77820974200b9341f473
Marium-sultana <mariumsultana073@gmail.com>
2021-09-23 08:47:35 +0600

    complete header and body compponent

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from '../../images/logo.png';
+import './Header.css';
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
-            <img src={logo} alt=""/>
+        <div className="header">
+            <img className="logo" src={logo} alt=""/>
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order Review</a>
+                <a href="/inventory">Manager Inventory</a>
+            </nav>
         </div>
     );
 };
