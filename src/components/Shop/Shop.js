3254f26aaafac9749ac13290dbef3fd8de759e4f
Marium-sultana <mariumsultana073@gmail.com>
2021-09-24 10:10:42 +0600

    local storage add

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,20 +2,42 @@ import React, { useState, useEffect } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
     useEffect(() => {
+        console.log('product API called')
         fetch('./products.JSON')
             .then(res => res.json())
-            .then(data => setProducts(data));
+            .then(data => {
+                setProducts(data);
+                console.log('Product revceived');
+                });
     }, [])
 
+    useEffect(() => {
+        console.log('L S Call')
+        if(products.length){
+            const savedCart = getStoredCart();
+            const storedCart = [];
+            // console.log(savedCart)
+            for (const key in savedCart) {
+                // console.log(key)
+                const addedProduct = products.find(product => product.key === key);
+                storedCart.push(addedProduct)
+            }
+            setCart(storedCart);
+        }
+    }, [products])
 
-    const handleAddToCart =(product) =>{
+        
+    const handleAddToCart = (product) => {
         // console.log(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        //save to local storage for now
+        addToDb(product.key)
     }
 
 
@@ -24,15 +46,15 @@ const Shop = () => {
             <div className="product-container">
                 <h3>Products:{products.length}</h3>
                 {
-                    products.map(product => <Product 
+                    products.map(product => <Product
                         key={product.key}
                         product={product}
-                        handleAddToCart = {handleAddToCart}
-                        ></Product> )
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
                 }
             </div>
             <div className="cart-container">
-               <Cart cart={cart}></Cart>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
